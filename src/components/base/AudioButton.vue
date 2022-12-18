082424b3ad70e179a8140424265b003a70300fa5
or-abdillh <hans.abdillh05@gmail.com>
2022-12-19 03:51:55 +0800

    Update audio url

--- src/components/base/AudioButton.vue
+++ src/components/base/AudioButton.vue
@@ -1,6 +1,6 @@
 <template>
     <audio ref="audioEl" autoplay>
-        <source src="https://drive.google.com/uc?id=1_co39PvvAifJLgsCETd0Rds8oFpoNBoK&export=download" type="audio/mp3">
+        <source src="https://drive.google.com/uc?id=1aosP7QNNU4be74Hou76aXeA-y26Wekbl&export=download" type="audio/mp3">
     </audio>
     <button style="height: 40px; width: 40px" 
         class="bg-yellow-400 bg-opacity-50 backdrop-blur-lg rounded-full fixed bottom-40 text-lg grid place-items-center left-3 text-gray-100 z-50 border border-gray-200" 

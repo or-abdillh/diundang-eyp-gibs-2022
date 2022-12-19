7fd9d7e638b06ecb20cceb319f4c89fa7e13f0b2
Oka R. Abdillah <67676285+or-abdillh@users.noreply.github.com>
2022-12-19 10:58:11 +0800

    Update Streaming.vue

--- src/components/main/section/Streaming.vue
+++ src/components/main/section/Streaming.vue
@@ -2,7 +2,7 @@
     <section class="px-5 py-16 bg-white bg-four-point-stars">
         <Card class="bg-sapphire" header-title="STREAMING">
             <section data-aos="zoom-in-up" >
-                <a href="https://youtube.com" target="_blank" class="btn border border-gray-200 bg-tambourine flex gap-2 items-center w-fit mx-auto">
+                <a href="https://www.youtube.com/@smp-smagibs1744" target="_blank" class="btn border border-gray-200 bg-tambourine flex gap-2 items-center w-fit mx-auto">
                     <i class="fa-brands fa-youtube text-red-600 text-3xl"></i>
                     <p class="text-xl font-serif font-medium text-gray-500">Live Streaming</p>
                 </a>
